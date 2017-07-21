5b91e483a988d126a37579730c16dd6bb67702cb
Dominic Ma <dominic.c.ma@gmail.com>
2017-07-21 16:42:26 -0700

    "Included ability to distinguish between American Express and Diner's club"

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,15 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  var cardArray = Array.from(cardNumber);
+
+  if(cardArray[0] == 3) {
+    if((cardArray[1] == 8 || cardArray[1] == 9) && cardArray.length === 14) {
+        return "Diner's Club";
+    } else if((cardArray[1] == 4 || cardArray[1] == 7) && cardArray.length === 15) {
+        return "American Express";
+    }
+  }
 };
 
 
